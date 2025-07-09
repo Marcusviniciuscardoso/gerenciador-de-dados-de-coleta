f04c7ba24e38a61f540d20b4dc617c50b2ad356e
Marcusviniciuscardoso <m.v.cardoso.r@gmail.com>
2025-07-09 03:55:01 -0300

    feat: sameSite-att

--- backend/controllers/credencialController.js
+++ backend/controllers/credencialController.js
@@ -57,10 +57,11 @@ module.exports = {
 
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: true,              // true se estiver rodando HTTPS
-                sameSite: 'Strict',
-                maxAge: 7 * 24 * 60 * 60 * 1000 // 7 dias
+                secure: true,
+                sameSite: 'none',
+                maxAge: 3600000,
             });
+
 
             // AUDITORIA
             await Auditoria.create({
