2c3c1e40db1cacc5cfcfe9695480f21202f03357
Marcusviniciuscardoso <m.v.cardoso.r@gmail.com>
2025-09-28 15:23:59 -0300

    refactor: change-audit-user-model

--- backend/models/auditoria.js
+++ backend/models/auditoria.js
@@ -10,10 +10,14 @@ module.exports = (sequelize) => {
       primaryKey: true,
       allowNull: false,
     },
-    usuario_id: {
-      type: DataTypes.INTEGER,
+    usuario: {
+      type: DataTypes.STRING(255),
       allowNull: false,
     },
+    /*usuario_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },*/
     acao: {
       type: DataTypes.STRING(255),
       allowNull: false,
