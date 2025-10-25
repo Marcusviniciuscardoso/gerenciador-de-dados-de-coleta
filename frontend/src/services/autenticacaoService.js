74b30474ff709409fc83dbcc818c6f927592a5e3
Marcusviniciuscardoso <m.v.cardoso.r@gmail.com>
2025-10-25 16:53:24 -0300

    fix: caminho de post da api de cadastrarUsuarioCompleto corrigido

--- frontend/src/services/autenticacaoService.js
+++ frontend/src/services/autenticacaoService.js
@@ -1,5 +1,5 @@
 import api from './api';
 
 export const cadastrarUsuarioCompleto = (dados) => {
-  return api.post('/cadastro-completo', dados);
+  return api.post('/cadastro/cadastro-completo', dados);
 };
