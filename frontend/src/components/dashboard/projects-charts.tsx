8ecd3f1a2528d7960e7e56fd4326ad42f1e1b870
Marcusviniciuscardoso <m.v.cardoso.r@gmail.com>
2026-01-17 00:19:47 -0300

    fix: removendo quadro desnecessário

--- frontend/src/components/dashboard/projects-charts.tsx
+++ frontend/src/components/dashboard/projects-charts.tsx
@@ -400,15 +400,15 @@ export function ProjectsCharts({ data }: ProjectsChartsProps) {
       </div>
 
       {/* ========== Card: Evolução Temporal dos Projetos (Linha) ========== */}
-      <div className="w-[41rem] p-10 rounded-3xl text-slate-600 bg-gradient-to-bl from-slate-200 to-slate-50
+      {/*<div className="w-[41rem] p-10 rounded-3xl text-slate-600 bg-gradient-to-bl from-slate-200 to-slate-50
                      grid grid-cols-[1fr_auto] gap-6
                      [box-shadow:inset_-2px_2px_0_rgba(255,255,255,1),_-20px_20px_40px_rgba(0,0,0,.25)]">
-        <h3 className="text-2xl font-medium uppercase tracking-wide self-end">Programming</h3>
+        <h3 className="text-2xl font-medium uppercase tracking-wide self-end">Programminng</h3>
         <div className="text-5xl leading-none">
           <i className="fa-solid fa-laptop-code bg-gradient-to-r from-rose-500 to-indigo-500 bg-clip-text text-transparent"></i>
         </div>
 
-        {/*<div className="col-span-2">
+        <div className="col-span-2">
           <h4 className="text-lg font-semibold mb-2">Evolução dos projetos</h4>
           <div className="h-[300px]">
             <ResponsiveContainer width="100%" height="100%">
@@ -422,10 +422,10 @@ export function ProjectsCharts({ data }: ProjectsChartsProps) {
               </LineChart>
             </ResponsiveContainer>
           </div>
-        </div>*/}
+        </div>
 
-        <div className="col-span-2 h-[2px] bg-gradient-to-r from-rose-500 to-indigo-500"></div>
-      </div>
+       <div className="col-span-2 h-[2px] bg-gradient-to-r from-rose-500 to-indigo-500"></div>
+      </div>*/}
     </div>
   );
 }
